960ddc3fa858ea356cbd5952394ba7f6ce63c323
tantm689 <tramita2712004@gmail.com>
2026-07-22 17:55:59 +0700

    update

--- prisma/migrations/20260722064341_add_sbt_answer_fields/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Lesson" ADD COLUMN "sbtAnswerCutPath" TEXT;
-ALTER TABLE "Lesson" ADD COLUMN "sbtAnswerPageEnd" INTEGER;
-ALTER TABLE "Lesson" ADD COLUMN "sbtAnswerPageStart" INTEGER;
